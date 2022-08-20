69aa38cd1e6c5e29d5dd53ffe04e46acdfd77839
Amad Ul Hassan <amadhashmi179@gmail.com>
2022-08-20 23:03:05 +0500

    send data to db
    sent data to db

--- backend/app.js
+++ backend/app.js
@@ -8,18 +8,38 @@ const api = process.env.API_URL
 // middleware
 app.use(express.json())
 app.use(morgan('tiny'))
-app.get(`${api}/products`, (req, res)=>{
-    const product = {
-        id: 1,
-        name: "Amad Ul Hassan",
-        image: "some_url"
+
+const productSchema = mongoose.Schema({
+    name: String,
+    image: String, 
+    countInStock: {type:Number,required: true}
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+app.get(`${api}/products`, async (req, res)=>{
+    const productList = await Product.find();
+    if(!productList){
+        res.status(500).json({
+            status: false
+        })
     }
-    res.send(product)
+    res.send(productList)
 });
 app.post(`${api}/products`, (req, res)=>{
-    const newProduct = req.body;
-    console.log(newProduct);
-    res.send(newProduct)
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock
+    })
+    product.save().then((createdProduct)=>{
+        res.status(201).json(createdProduct)
+    }).catch((err)=>{
+        res.status(500).json({
+            error: err,
+            success: false
+        })
+    })
 })
 
 mongoose.connect(process.env.CONNECTION_STRING)
