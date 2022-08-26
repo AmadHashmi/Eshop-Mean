7e73b4293fbd3070e2799ab593d78d57518d9a10
Amad Ul Hassan <amadhashmi179@gmail.com>
2022-08-27 00:49:03 +0500

    updated routes
    updated routes

--- company/learnmean/apps/shop/src/app/app.module.ts
+++ company/learnmean/apps/shop/src/app/app.module.ts
@@ -6,18 +6,22 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 const routes: Routes = [
     {
-        path: '', component: HomePageComponent,
+        path: '',
+        component: HomePageComponent
     },
     {
-        path: 'products', component: ProductListComponent
+        path: 'products',
+        component: ProductListComponent
     }
-]
+];
 @NgModule({
-    declarations: [AppComponent, NxWelcomeComponent, HomePageComponent, ProductListComponent],
+    declarations: [AppComponent, NxWelcomeComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
     imports: [BrowserModule, RouterModule.forRoot(routes)],
     providers: [],
     bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
