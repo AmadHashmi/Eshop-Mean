630dae4bfbffa80c2aafde60d0783c2fdb7e644b
Amad Ul Hassan <amadhashmi179@gmail.com>
2022-08-30 00:15:47 +0500

    get and create categories
    get and create categories

--- company/learnmean/apps/admin/src/app/app.module.ts
+++ company/learnmean/apps/admin/src/app/app.module.ts
@@ -1,47 +1,71 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { RouterModule, Routes } from "@angular/router";
+import { ShellComponent } from "./shared/shell/shell.component";
+import { SidebarComponent } from "./shared/sidebar/sidebar.component";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { CategoriesListComponent } from "./categories/categories-list/categories-list.component";
 
-import { AppComponent } from './app.component';
-import { RouterModule, Routes } from '@angular/router';
-import { ShellComponent } from './shared/shell/shell.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { CategoriesListComponent } from './categories/categories-list/categories-list.component';
-
-import { CardModule } from 'primeng/card';
-import { ToolbarModule } from 'primeng/toolbar';
-import { ButtonModule } from 'primeng/button';
-import { TableModule } from 'primeng/table'
-import { HttpClientModule } from '@angular/common/http';
-import { CategoriesService } from '@learnmean/products';
-import { CategoryFormComponent } from './categories/category-form/category-form.component';
-const UX_MODULES = [CardModule, ToolbarModule, ButtonModule, TableModule];
+import { CardModule } from "primeng/card";
+import { ToolbarModule } from "primeng/toolbar";
+import { ButtonModule } from "primeng/button";
+import { TableModule } from "primeng/table";
+import { HttpClientModule } from "@angular/common/http";
+import { CategoriesService } from "@learnmean/products";
+import { CategoryFormComponent } from "./categories/category-form/category-form.component";
+import { InputTextModule } from "primeng/inputtext";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ToastModule } from "primeng/toast";
+import { MessageService } from "primeng/api";
+const UX_MODULES = [
+  CardModule,
+  InputTextModule,
+  ToolbarModule,
+  ButtonModule,
+  TableModule,
+  ToastModule,
+];
 const routes: Routes = [
-    {
-        path: '', component: ShellComponent, children: [
-            {
-                path: 'dashboard',
-                component: DashboardComponent
-            },
-            {
-                path: 'categories',
-                component: CategoriesListComponent
-            },
-            {
-                path: 'categories/form',
-                component: CategoryFormComponent
-            }
-
-        ]
-    }
-]
+  {
+    path: "",
+    component: ShellComponent,
+    children: [
+      {
+        path: "dashboard",
+        component: DashboardComponent,
+      },
+      {
+        path: "categories",
+        component: CategoriesListComponent,
+      },
+      {
+        path: "categories/form",
+        component: CategoryFormComponent,
+      },
+    ],
+  },
+];
 @NgModule({
-    declarations: [AppComponent, ShellComponent, SidebarComponent, DashboardComponent, CategoriesListComponent, CategoryFormComponent],
-    imports: [BrowserModule,
-        HttpClientModule,
-        RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
-        ...UX_MODULES],
-    providers: [CategoriesService],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    ShellComponent,
+    SidebarComponent,
+    DashboardComponent,
+    CategoriesListComponent,
+    CategoryFormComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes, { initialNavigation: "enabledBlocking" }),
+    ...UX_MODULES,
+  ],
+  providers: [CategoriesService, MessageService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- company/learnmean/libs/products/src/lib/services/categories.service.ts
+++ company/learnmean/libs/products/src/lib/services/categories.service.ts
@@ -1,15 +1,21 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import { Observable } from 'rxjs';
-import { Category } from '../models/categories';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Category } from "../models/categories";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CategoriesService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getCategories(): Observable<Category[]> {
-    return this.http.get<Category[]>('http://localhost:3000/api/v1/categories')
+    return this.http.get<Category[]>("http://localhost:3000/api/v1/categories");
+  }
+
+  createCategory(category: Category): Observable<Category> {
+    return this.http.post<Category>(
+      "http://localhost:3000/api/v1/categories",
+      category
+    );
   }
 }
