555de183e1b226b4078b40539cc90f4c2f09fa1b
Amad Ul Hassan <amadhashmi179@gmail.com>
2022-10-21 15:36:53 +0200

    showing featured products
    showing featured products

--- company/learnmean/libs/products/src/lib/components/featured-products/featured-products.component.ts
+++ company/learnmean/libs/products/src/lib/components/featured-products/featured-products.component.ts
@@ -1,12 +1,32 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subject, takeUntil } from "rxjs";
+import { Product } from "../../models/products";
+import { ProductsService } from "../../services/products.service";
 
 @Component({
   selector: "products-featured-products",
   templateUrl: "./featured-products.component.html",
   styles: [],
 })
-export class FeaturedProductsComponent implements OnInit {
-  constructor() {}
+export class FeaturedProductsComponent implements OnInit, OnDestroy {
+  featuredProducts: Product[] = [];
+  endSubs$: Subject<any> = new Subject();
+  constructor(private productsService: ProductsService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this._getFeaturedProducts();
+  }
+
+  private _getFeaturedProducts() {
+    this.productsService
+      .getFeaturedProducts(4)
+      .pipe(takeUntil(this.endSubs$))
+      .subscribe((products) => {
+        this.featuredProducts = products;
+      });
+  }
+  ngOnDestroy(): void {
+    this.endSubs$.next(null);
+    this.endSubs$.complete();
+  }
 }
