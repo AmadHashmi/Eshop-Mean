89647dd51c6e6038a200f40453e478a948f36f74
Amad Ul Hassan <amadhashmi179@gmail.com>
2022-10-21 15:18:34 +0200

    featured items structure
    featured items structure

---
+++ company/learnmean/libs/products/src/lib/components/featured-products/featured-products.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "products-featured-products",
+  templateUrl: "./featured-products.component.html",
+  styles: [],
+})
+export class FeaturedProductsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- company/learnmean/libs/products/src/lib/products.module.ts
+++ company/learnmean/libs/products/src/lib/products.module.ts
@@ -3,10 +3,23 @@ import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { ProductsSearchComponent } from "./components/products-search/products-search.component";
 import { CategoriesBannerComponent } from "./components/categories-banner/categories-banner.component";
+import { ProductItemComponent } from "./components/product-item/product-item.component";
+import { FeaturedProductsComponent } from "./components/featured-products/featured-products.component";
+import { ButtonModule } from "primeng/button";
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
-  declarations: [ProductsSearchComponent, CategoriesBannerComponent],
-  exports: [ProductsSearchComponent, CategoriesBannerComponent],
+  imports: [CommonModule, RouterModule, ButtonModule],
+  declarations: [
+    ProductsSearchComponent,
+    CategoriesBannerComponent,
+    ProductItemComponent,
+    FeaturedProductsComponent,
+  ],
+  exports: [
+    ProductsSearchComponent,
+    CategoriesBannerComponent,
+    ProductItemComponent,
+    FeaturedProductsComponent,
+  ],
 })
 export class ProductsModule {}
